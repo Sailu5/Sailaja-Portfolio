3bb5cd7d2a70cb10f3a2d253d5a772f27705b0a1
Sailu5 <cherlopallisailaja05@gmail.com>
2025-07-20 22:25:40 +0530

    my commit changes

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import AboutMee from "./components/sections/AboutMee";
+import Skills from "./components/sections/Skills";
+
+import { AnimatePresence } from "framer-motion";
+import Education from "./components/sections/Education";
+
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
+import ProjectDetails from "./components/Dialog/ProjectDetails";
+import { useState } from "react";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(255, 85, 0, 0.15) 0%,
+      rgba(7, 198, 237, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(58, 220, 241, 0) 50%,
+      rgba(104, 245, 245, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+
+          <AnimatePresence>
+            <div>
+              <AboutMee />
+              <Wrapper>
+                <Skills />
+
+              </Wrapper>
+              <Projects openModal={openModal} setOpenModal={setOpenModal} />
+              <Wrapper>
+                <Education />
+                <Contact />
+              </Wrapper>
+              <Footer />
+
+              {openModal.state && (
+                <ProjectDetails
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                />
+              )}
+            </div>
+          </AnimatePresence>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,234 @@
+export const Bio = {
+  name: "Sailaja Cherlopalli",
+  roles: [
+    "Frontend Developer",
+    "Machine Learning",
+    "Data Science",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Sailu5",
+  resume:
+    "https://drive.google.com/file/d/1-wVJC-9JeJPdLZDi-jtp96VWrrA1QBNZ/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/cherlopallisailaja/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+
+    ],
+  },
+
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://avatars.githubusercontent.com/u/15658638?s=280&v=4",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://avatars.githubusercontent.com/u/33467679?s=280&v=4",
+      },
+ 
+    ],
+  },
+  {
+    title:"Programming Languages",
+    skills : [
+      {
+        name : "Java",
+        image :"https://education.oracle.com/file/general/p-80-java.png"
+      },
+      {
+         name : "Python",
+         image :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBgY4Wfy9hXIn-GOD3xvwiisF8GM97MiktoA&s"
+      },
+      {
+        name :"C Programming",
+        image : "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg"
+      }
+     
+    ]
+
+
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name : "Codepen",
+        image : "https://blog.codepen.io/wp-content/uploads/2012/06/Button-Black-Large.png"
+      }
+
+
+    ],
+  },
+
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https:cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png"
+      },
+
+    ],
+  },
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB8dlTBWhuObJLmjy5ERDfiErsQtjFDzF0CQ&s",
+    school: "Rajeev Gandhi Memorial College of Engineering and Technology, Nandyal",
+    date: "Oct 2022 - Apr 2026",
+    grade: "8.97 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering Specialization of Data Science at Rajeev Gandhi Memorial College of Engineering and Technology, Nandyal. I have completed 6 semesters and have a CGPA of 8.97. I learn skills from web those are Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am Web-dev lead in Google Developers Group Club (GDGC) at RGMCET, and also have another community i.e, Women in Data Science (WiDS) World Wide Ambassador.", 
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering(Data Science)",
+  },
+  {
+    id: 1,
+    img: "https://bingo.icbse.com/v1/business.jpg?action=logo&id=gdvk0n",
+    school: "Penchal Reddy High School(CBSE),Rayachoty",
+    date: "Jun 2020 -Jun 2022",
+    grade: "86.7%",
+    desc: "I completed my class 12 high school education at Penchal Reddy High School(CBSE), Rayachoty, where I studied MPC(Maths Physics and Chemistry).",
+    degree: "CBSE(XII),PCM",
+  },
+  {
+    id: 2,
+    img: "https://play-lh.googleusercontent.com/fUybm6eIklityenGlp6sFk2zUfY1VJsYe8hss9kJ_WK9Tvlhv3g0WAdlZTS3aVdkDs8=w600-h300-pc0xffffff-pd",
+    school: "Raju High School, Rayachoty",
+    date: "Apr 2019 - Apr 2020",
+    grade: "100%",
+    desc: "I completed my class 10 education at Raju High School, Rayachoty, where i Studied My Bsics for Future.",
+    degree: "SSC(X), Senior Secondary Education",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "E-Commerece Website",
+    date: "Apr 2024 - Jun 2024",
+    description:
+      "Built a laptop-focused e-commerce site using React.js and deployed it via GitHub Pages. Integrated user login, cart, and address modules for end-to-end shopping flow.  ",
+    image:
+      "https://www.webibazaar.com/image/cache/catalog/product/52008005-shopeur-electronics-wooCommerce-theme/00-497x497.jpg",
+    tags: [
+      "React Js",
+      "MangoDB",
+      "Node Js",
+      "Express Js",
+     
+    ],
+    category: "web app",
+    github: "https://github.com/Sailu5/Ecommerce-MERN-Stack",
+    webapp: "https://mern-shop-abxs.onrender.com/",
+  },
+
+  {
+    id : 12,
+    title: "Amazon Product Analysis Using Sentiment Analysis",
+    date :"Jan 2025 - Feb 2025",
+    description: "Developed a sentiment analysis model to evaluate customer reviews on Amazon products, providing insights into product performance and customer satisfaction",
+    image: "https://editor.analyticsvidhya.com/uploads/55259main.png",
+    tags: [
+      'python',
+      'NLP',
+      'Streamlit',
+      'Sentiment Analysis'
+    ],
+    category: "Machine Learning",
+    github:"https://github.com/Sailu5/Amazon-Product-Review-Sentiment-Analysis-NLP-Project",
+
+  },
+
+  {
+    id: 13,
+    title: "Medical Insurance Cost Prediction",
+    date: "May 2024 - Jul 2024",
+    description: "Developed a machine Learning model to predict medical insurance costs based on patient data, enabling better financial planning for healthcare providers.",
+    image:"https://repository-images.githubusercontent.com/639115640/ef1cceea-f3a3-448f-a44e-530f55a48aa5",
+    tags: [
+      "python",
+      "Machine Learning",
+      "Streamlit",
+      "Data Visualization",
+    
+    ],
+    category: "Machine Learning",
+    github:"https://github.com/Sailu5/ML-project"
+  }
+
+];
+
